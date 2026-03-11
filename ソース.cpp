2e8b537b59a6bd0caf728ea9f1ec7d8c55dfefdf
YY <266007415+YY20260306@users.noreply.github.com>
2026-03-12 02:56:04 +0900

    ループ条件を実装。とりあえず動いたので一旦完成。

--- ソース.cpp
+++ ソース.cpp
@@ -6,7 +6,23 @@ int main() {
 	std::srand(static_cast<unsigned int>(std::time(nullptr))); //時刻を乱数の種として取得→型を符号なし整数に変換
 	int answer = std::rand() % 100 + 1; //100で割った余り0～99に+1することで1～100の乱数を取得
 
-	int guess; //解答者の入力を受け取る変数
-	std::cout << "正解だと思う数字を入力してください。" << std::endl;
-	std::cin >> guess;
+	int guess = 1; //解答者の入力を受け取る変数
+
+	for (int i = 1; answer != guess; i++) {
+		std::cout << "正解だと思う数字を入力してください。" << std::endl;
+		std::cin >> guess;
+
+		if (answer > guess) {
+			std::cout << "もっと大きいです。" << std::endl;
+		}
+		else if (answer < guess) {
+			std::cout << "もっと小さいです。" << std::endl;
+		}
+		else {
+			std::cout << "正解です。おめでとう！" << std::endl;
+			std::cout << i << "回目の入力で正解しました！" << std::endl;
+		}
+
+	}
+return 0;
 }
