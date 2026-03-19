81d9df954f6ab8961fda3718f88774a14ec8dcbf
YY <266007415+YY20260306@users.noreply.github.com>
2026-03-20 04:29:05 +0900

    数字以外の入力があった際の挙動がうまくいかなかったエラーを修正。

--- ソース.cpp
+++ ソース.cpp
@@ -3,8 +3,45 @@
 #include <cstdlib>
 
 void PlayGame() {
+	std::srand(static_cast<unsigned int>(std::time(nullptr))); //時刻を乱数の種として取得→型を符号なし整数に変換
+	const int max_number = 10;
+	int answer = std::rand() % max_number + 1; //100で割った余り0～99に+1することで1～100の乱数を取得
+	const int give_up = 0;
 
+	int guess = 0; //解答者の入力を受け取る変数
+	int count = 0; //解答した回数のカウント
+	while (true) {
+		std::cout << "1から" << max_number << "の中で正解だと思う数字を入力してください。" << std::endl;
+		std::cout << "ゲームをやり直すには" << give_up << "を入力してください。\n";
+		std::cin >> guess;
+
+		if (std::cin.fail()) {
+			std::cout << "数字以外を入力しないでください（怒）。" << std::endl;
+			std::cin.clear();
+			std::cin.ignore(1024, '\n');
+			continue;
+		} //まず最初に入力エラーをはじく（ガード条件？）
+
+		else if (guess == give_up) {
+			break;
+		} //0を入力した際にやり直し
+
+		count++;
+
+		if (answer > guess) {
+			std::cout << "もっと大きいです。" << std::endl;
+		}
+		else if (answer < guess) {
+			std::cout << "もっと小さいです。" << std::endl;
+		}
+		else {
+			std::cout << "正解です。おめでとう！\n"; //\nの場合は文字列の中に含む
+			std::cout << count << "回目の入力で正解しました！" << std::endl;
+			break; //これでループを抜けてAskRetry実行
+		}
+	}
 }
+
 
 bool AskRetry() {
 	char retry; //リトライするかの入力を受け取る変数
@@ -30,45 +67,7 @@ bool AskRetry() {
 int main() {
 
 	do {
-		std::srand(static_cast<unsigned int>(std::time(nullptr))); //時刻を乱数の種として取得→型を符号なし整数に変換
-		const int max_number = 10;
-		int answer = std::rand() % max_number + 1; //100で割った余り0～99に+1することで1～100の乱数を取得
-		const int give_up = 0;
-
-		int guess = 0; //解答者の入力を受け取る変数
-		int count = 0; //解答した回数のカウント
-		while (true) {
-			std::cout << "1から" << max_number << "の中で正解だと思う数字を入力してください。" << std::endl;
-			std::cout << "ゲームをやり直すには" << give_up << "を入力してください。\n";
-			std::cin >> guess;
-
-			if (guess == give_up) {
-				break;
-			}
-
-			else if (std::cin.fail()) {
-				std::cout << "数字以外を入力しないでください（怒）。" << std::endl;
-				std::cin.clear();
-				std::cin.ignore(1024, '\n');
-				continue;
-			}
-
-			count++;
-
-			if (answer > guess) {
-				std::cout << "もっと大きいです。" << std::endl;
-			}
-			else if (answer < guess) {
-				std::cout << "もっと小さいです。" << std::endl;
-			}
-			else {
-				std::cout << "正解です。おめでとう！\n"; //\nの場合は文字列の中に含む
-				std::cout << count << "回目の入力で正解しました！" << std::endl;
-				break; //これでループを抜けてAskRetry実行
-			}
-		}
-
-
+		PlayGame();
 	} while (AskRetry());
 	return 0;
 }
